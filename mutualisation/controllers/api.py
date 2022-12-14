8659014df5bb6fc0f06304255919ae2208a25057
ma@syentys.fr <ma@syentys.fr>
2022-12-14 10:59:21 +0100

    [IMP] anomalie form view

--- mutualisation/controllers/api.py
+++ mutualisation/controllers/api.py
@@ -4,7 +4,7 @@ from odoo.http import request, Response, JsonRequest
 class ApiController(http.Controller):
 
     @http.route('/web/authenticate_mobile', type='json', auth="none")
-    def authenticate(self, db, login, password, employe=None, pin=None, base_location=None):
+    def authenticate_mobile(self, db, login, password, employe=None, pin=None):
         request.params['emp'] = employe
         request.params['pin'] = pin
         request.session.authenticate(db, login, password)
